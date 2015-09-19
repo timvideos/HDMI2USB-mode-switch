a29abdaeba78d0c0a2acc89cba18b8dd3112923e
Tim 'mithro' Ansell <mithro@mithis.com>
2015-09-19 23:01:44 +1000

    Start getting ready for detach support.

--- find_board.py
+++ find_board.py
@@ -13,11 +13,37 @@ import os
 from collections import namedtuple
 import logging
 
+PathBase = namedtuple('PathBase', ['bus', 'address'])
+class Path(PathBase):
+    def __init__(self, *args, **kw):
+        PathBase.__init__(self, *args, **kw)
+        assert os.path.exists(self.path)
+
+    @property
+    def path(self):
+        return '/dev/bus/usb/%03x/%03x' % (self.bus, self.address)
+
+    def __str__(self):
+        return self.path
+
+    def __cmp__(self, other):
+        if isinstance(other, Path):
+            return cmp(self.path, other.path)
+        return cmp(self.path, other)
+
+
 Device = namedtuple('Device', ['path', 'vid', 'pid', 'serial'])
 Device.__str__ = lambda self: "Device(%04x:%04x %r)" % (self.vid, self.pid, self.serial)
 #Device.__cmp__ = lambda a, b: cmp(a.path, b.path)
 
 def find_usb_devices_libusb():
+    class LibDevice(Device):
+        def detach(self):
+            # Detach any driver currently attached.
+            dev = usb.core.find(bus=self.path.bus, address=self.path.address)
+            if dev.is_kernel_driver_active():
+                dev.detach_kernel_driver()
+
     import usb
     if usb.__file__.endswith('.so'):
         logging.warning("Your python usb module is old.")
@@ -46,7 +72,7 @@ def find_usb_devices_lsusb():
 
     # 'Bus 002 Device 002: ID 8087:0024 Intel Corp. Integrated Rate Matching Hub'
     lsusb_device_regex = re.compile(
-        "Bus (?P<bus>[0-9]+) Device (?P<device>[0-9a-f]+):"
+        "Bus (?P<bus>[0-9]+) Device (?P<address>[0-9a-f]+):"
         " ID (?P<vid>[0-9a-f]+):(?P<pid>[0-9a-f]+)")
     # ' iSerial                 0 '
     # ' iSerial                 3 04c321e2df0e8bb2'
@@ -55,40 +81,80 @@ def find_usb_devices_lsusb():
         re.MULTILINE) 
 
     class LsusbDevice(Device):
+        def __new__(cls, *args, **kw):
+            return Device.__new__(cls, *args, serial=None, **kw)
+
         @property
         def serial(self):
             # Get the serial number from the device and cache it.
             if not hasattr(self, "_serial"):
-                output = subprocess.check_output('lsusb -D %s' % self.path, shell=True, stderr=subprocess.STDOUT)
+                output = subprocess.check_output('lsusb -D %s' % (self.path,), shell=True, stderr=subprocess.STDOUT)
                 bits = lsusb_serial_regex.search(output)
                 assert bits, repr(output)
                 self._serial = bits.group("serial")
             return self._serial
+
+        def detach(self):
+            pass
 
     devobjs = []
     output = subprocess.check_output('lsusb')
     for line in output.splitlines():
         bits = lsusb_device_regex.match(line)
         assert bits, repr(line)
-        path = "/dev/bus/usb/%s/%s" % (bits.group('bus'), bits.group('device'))
-        assert os.path.exists(path), path
+
         vid = int(bits.group('vid'), base=16)
         pid = int(bits.group('pid'), base=16)
-        devobjs.append(LsusbDevice(vid=vid, pid=pid, serial=None, path=path))
+        bus = int(bits.group('bus'), base=16)
+        address = int(bits.group('address'), base=16)
+        devobjs.append(LsusbDevice(
+            vid=vid, pid=pid, path=Path(bus=bus, address=address),
+            ))
 
     return devobjs
 
 
-libusb_devices = find_usb_devices_libusb()
-lsusb_devices = find_usb_devices_lsusb()
+def test_libusb_and_lsusb_equal():
+    libusb_devices = find_usb_devices_libusb()
+    lsusb_devices = find_usb_devices_lsusb()
+    for libobj, lsobj in zip(sorted(libusb_devices), sorted(lsusb_devices)):
+        print "%s -- lib: %-40s ls: %-40s" % (libobj.path, libobj, lsobj)
+        assert libobj.vid == lsobj.vid, "%r == %r" % (libobj.vid, lsobj.vid)
+        assert libobj.pid == lsobj.pid, "%r == %r" % (libobj.pid, lsobj.pid)
+        #assert libobj.did == lsobj.did, "%r == %r" % (libobj.did, lsobj.did)
+        assert libobj.serial == lsobj.serial, "%r == %r" % (libobj.serial, lsobj.serial)
+        assert libobj.path == lsobj.path, "%r == %r" % (libobj.path, lsobj.path)
 
-for libobj, lsobj in zip(sorted(libusb_devices), sorted(lsusb_devices)):
-    print "%s -- lib: %-40s ls: %-40s" % (libobj.path, libobj, lsobj)
-    assert libobj.vid == lsobj.vid, "%r == %r" % (libobj.vid, lsobj.vid)
-    assert libobj.pid == lsobj.pid, "%r == %r" % (libobj.pid, lsobj.pid)
-    #assert libobj.did == lsobj.did, "%r == %r" % (libobj.did, lsobj.did)
-    assert libobj.serial == lsobj.serial, "%r == %r" % (libobj.serial, lsobj.serial)
-    assert libobj.path == lsobj.path, "%r == %r" % (libobj.path, lsobj.path)
+test_libusb_and_lsusb_equal()
 
-        
+
+import argparse
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--mac', help='Find board with the given MAC address.')
+parser.add_argument('--dna', help='Find board with the given Device DNA.')
+
+"""
+# Digilent Atlys board with stock "Adept" firmware
+# VID=
+
+  - Digilent Atlys board loaded with Nero-USB JTAG firmware
+  - Digilent Atlys board loaded with MakeStuff firmware
+  - Digilent Atlys board loaded with HDMI2USB "unconfigured" firmware
+  - Digilent Atlys board loaded with HDMI2USB "configured" firmware
+
+  - Unconfigured Cypress FX2, could be a Numato Opsis
+
+  - Numato Opsis board with no FX2 firmware loaded
+  - Numato Opsis board loaded with Nero-USB JTAG firmware
+  - Numato Opsis board loaded with MakeStuff firmware
+  - Numato Opsis board loaded with HDMI2USB firmware
+
+"""
+
+
+
+
+
+
+
 
