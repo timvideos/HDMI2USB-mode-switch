f6f9fbf1fd258942425752a09a0a8b3224e552a1
Tim 'mithro' Ansell <mithro@mithis.com>
2015-10-10 15:46:01 +1100

    Adding the USB-UART firmware for the Opsis.

--- update-fx2-firmware.sh
+++ update-fx2-firmware.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 
-if [ ! -d ixo-usb-jtag ]; then
-	git clone git://github.com/mithro/ixo-usb-jtag.git
-	cd ixo-usb-jtag
-else
-	cd ixo-usb-jtag
-	git pull
-fi
+set +x
+set +e
 
-fakeroot ./debian/rules binary
-cp ./output/hw_opsis.hex ../fx2-firmware/atlys/ixo-usb-jtag.hex
-cp ./output/hw_opsis.hex ../fx2-firmware/opsis/ixo-usb-jtag.hex
-cd ..
+TOP=$PWD
 
-git add fx2-firmware/atlys/ixo-usb-jtag.hex
-git add fx2-firmware/opsis/ixo-usb-jtag.hex
+cd build
+
+#### ixo-usb-jtag for JTAG operation
+(
+	if [ ! -d ixo-usb-jtag ]; then
+		git clone git://github.com/mithro/ixo-usb-jtag.git
+		cd ixo-usb-jtag
+	else
+		cd ixo-usb-jtag
+		git pull
+	fi
+
+	fakeroot ./debian/rules binary
+	cp ./output/hw_opsis.hex $TOP/fx2-firmware/atlys/ixo-usb-jtag.hex
+	cp ./output/hw_opsis.hex $TOP/fx2-firmware/opsis/ixo-usb-jtag.hex
+
+	cd $TOP
+	git add fx2-firmware/atlys/ixo-usb-jtag.hex
+	git add fx2-firmware/opsis/ixo-usb-jtag.hex
+)
+
+#### fx2lib firmware for USB serial operation
+(
+	if [ ! -d fx2lib ]; then
+		git clone git://github.com/mithro/fx2lib.git
+		git checkout cdc-usb-serialno-from-eeprom
+		cd fx2lib
+	else
+		cd fx2lib
+		git pull
+	fi
+
+	make
+	cp examples/cdc/to-uart/build/cdc-acm-to-uart.ihx $TOP/fx2-firmware/opsis/usb-uart.ihx
+	cd $TOP
+	git add fx2-firmware/opsis/usb-uart.ihx
+)
+
