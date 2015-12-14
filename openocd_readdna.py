37c529d8ff6a2b4b767b9d0b058c2fd92a12b756
Tim 'mithro' Ansell <mithro@mithis.com>
2015-12-14 17:21:38 +1100

    Tool which uses openocd to generate a DNA barcode.

---
+++ openocd_readdna.py
@@ -0,0 +1,30 @@
+
+
+import subprocess
+import re
+
+import argparse
+
+
+
+output = subprocess.check_output("""\
+openocd --file board/%s.cfg -c "init; xc6s_print_dna xc6s.tap; exit"
+""" % "pipistrello", shell=True, stderr=subprocess.STDOUT)
+
+for line in output.splitlines():
+    line = line.decode('utf-8')
+    if not line.startswith('DNA = '):
+       continue
+
+    dna = re.match("DNA = ([10]*) \((0x[01-9a-f]*)\)", line)
+    binv, hexv = dna.groups()
+    binc = int(binv, 2)
+    hexc = int(hexv, 16)
+    assert binc == hexc
+
+    hexs = hex(hexc)
+    import barcode
+    b = barcode.get('Code128', hexs[2:])
+    b.save('barcode_dna', {'module_height': 10, 'module_width': 0.25, 'font_size': 12, 'text_distance': 5, 'human': 'DNA - %s' % hexs})
+    
+    
