02cb680c28f5375039e7f1870ef5ad8530b663c1
Tim 'mithro' Ansell <mithro@mithis.com>
2016-06-02 16:07:58 +1000

    Replace flash script with update version.

--- opsis-flash.sh
+++ opsis-flash.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
-python ./opsis_eeprom_prog.py --go
-sleep 1
-python ./opsis_eeprom_prog.py
-./hdmi2usb-mode-switch.py --mode=serial
-./hdmi2usb-mode-switch.py --mode=jtag
-./hdmi2usb-mode-switch.py --flash-gateware=spiflash/opsis_hdmi2usb-hdmi2usbsoc-opsis.bin --verbose
+./hdmi2usb-mode-switch.py --mode=serial -v
+./hdmi2usb-mode-switch.py --mode=jtag -v
+./hdmi2usb-mode-switch.py --mode=serial -v
+./hdmi2usb-mode-switch.py --mode=jtag -v
+./hdmi2usb-mode-switch.py --flash-gateware=$1 --verbose
--- opsis-update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-./hdmi2usb-mode-switch.py --mode=serial -v
-./hdmi2usb-mode-switch.py --mode=jtag -v
-./hdmi2usb-mode-switch.py --mode=serial -v
-./hdmi2usb-mode-switch.py --mode=jtag -v
-./hdmi2usb-mode-switch.py --flash-gateware=$1 --verbose
