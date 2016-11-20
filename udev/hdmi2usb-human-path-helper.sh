6c66691f452b7e3f91c830a24054c306bd0fb00e
Stefano Rivera <stefano@rivera.za.net>
2016-11-20 15:50:53 +0100

    There are no bashisms, use /bin/sh

--- udev/hdmi2usb-human-path-helper.sh
+++ udev/hdmi2usb-human-path-helper.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ x"$1" = xtest ]; then
 	# DEVPATH=/devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-4/3-4.3/3-4.3:1.2/tty/ttyACM0
 	# ID_PATH=pci-0000:08:00.0-usb-0:4.3:1.2
 	# ID_PATH_HUMAN=usb.bus0.port4-hub.port3
-	./$0 /devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-4/3-4.3/3-4.3:1.2/tty/ttyACM0 
+	./$0 /devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-4/3-4.3/3-4.3:1.2/tty/ttyACM0
 
 	# DEVPATH=/devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-3/3-3:1.2/tty/ttyACM0
 	# ID_PATH=pci-0000:08:00.0-usb-0:3:1.2
