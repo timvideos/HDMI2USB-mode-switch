4447f262dcdce70565f2d8a334e5137c786c119a
Stefano Rivera <stefano@rivera.za.net>
2016-11-22 10:31:12 +0000

    Add a test that runs helpers under posh

--- udev/Makefile
+++ udev/Makefile
@@ -38,6 +38,11 @@ check:
 	@if ! id | grep -qF '(video)'; then \
 		echo "Not a member of the video group"; exit 1; \
 	fi
+	@for HELP in hdmi2usb-*-helper.sh; do \
+		echo "Checking $$HELP.."; \
+		SHELL=/bin/posh posh $$HELP test || exit 1; \
+		echo " Good!"; \
+	done
 
 uninstall:
 	sudo rm -f /etc/udev/rules.d/*hdmi2usb*
--- udev/hdmi2usb-human-path-helper.sh
+++ udev/hdmi2usb-human-path-helper.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 if [ x"$1" = xtest ]; then
+	if [ x"$SHELL" = x ]; then
+		SHELL=/bin/sh
+	fi
 	# DEVPATH=/devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-4/3-4.3/3-4.3:1.2/tty/ttyACM0
 	# ID_PATH=pci-0000:08:00.0-usb-0:4.3:1.2
 	# ID_PATH_HUMAN=usb.bus0.port4-hub.port3
-	./$0 /devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-4/3-4.3/3-4.3:1.2/tty/ttyACM0
+	$SHELL $0 /devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-4/3-4.3/3-4.3:1.2/tty/ttyACM0
 
 	# DEVPATH=/devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-3/3-3:1.2/tty/ttyACM0
 	# ID_PATH=pci-0000:08:00.0-usb-0:3:1.2
 	# ID_PATH_HUMAN=usb.bus0.port3
-	./$0 /devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-3/3-3:1.2/tty/ttyACM0
+	$SHELL $0 /devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-3/3-3:1.2/tty/ttyACM0
 
 	# DEVPATH=/devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-4/3-4.4
-	./$0 /devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-4/3-4.4
+	$SHELL $0 /devices/pci0000:00/0000:00:1c.7/0000:08:00.0/usb3/3-4/3-4.4
 
 	exit
 fi
